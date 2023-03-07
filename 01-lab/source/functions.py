0642b7957eba27557675895af1e37bbc31cd7343
ZaharKaras <zahar.karas@gmail.com>
2023-03-07 17:27:51 +0300

    feat: add function

---
+++ 01-lab/source/functions.py
@@ -0,0 +1,10 @@
+def calculate(num1, num2, operator):
+    if(operator == "add"):
+        return num1+num2
+    elif(operator == "sub"):
+        return num1-num2
+    elif(operator == "mult"):
+        return num1*num2
+    else:
+        return num1/num2
+    
