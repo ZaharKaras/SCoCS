19bb27e664200195469d5e78e6e7cc072d880177
ZaharKaras <zahar.karas@gmail.com>
2023-03-07 17:27:51 +0300

    feat: add function

--- 01-lab/source/main.py
+++ 01-lab/source/main.py
@@ -1,1 +1,12 @@
+from functions import calculate
+
 print("Hello world")
+
+print("Enter number_1")
+num1 = float(input())
+print("Enter number_2")
+num2 = float(input())
+print("Enter operation(add, sub, mult, div)")
+operator = input()
+result = calculate(num1, num2, operator)
+print("Reuslt: " + str(result))
