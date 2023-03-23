85845a86d6e22e8aa490eff5d159ff44f6087eda
ZaharKaras <zahar.karas@gmail.com>
2023-03-23 12:41:08 +0300

    feat: added exception

--- 01-lab/source/main.py
+++ 01-lab/source/main.py
@@ -2,14 +2,19 @@ from functions import calculate
 
 print("Hello world")
 
-print("Enter numbers and operator")
-num1 = float(input())
-print("Enter number_2")
-num2 = float(input())
-print("Enter operation(add, sub, mult, div)")
-operator = input()
-result = calculate(num1, num2, operator)
-print("Reuslt: " + str(result))
+try:
+    print("Enter numbers and operator")
+    num1 = float(input())
+    print("Enter number_2")
+    num2 = float(input())
+    print("Enter operation(add, sub, mult, div)")
+    operator = input()
+    result = calculate(num1, num2, operator)
+    print("Reuslt: " + str(result))
+except Exception as exp:
+    print(exp.args)
+    
+
 
 numbers = [1,2,3,4,5,6,7,8,9,10]
 
