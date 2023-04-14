0274b70092222cdb64f43e03fa8bb4bf3502ef81
ZaharKaras <zahar.karas@gmail.com>
2023-04-14 17:21:59 +0300

    Feat: added avg word length

--- 02-lab/Task_1/source/functions.py
+++ 02-lab/Task_1/source/functions.py
@@ -10,3 +10,8 @@ def avg_sentence_length(text):
     sentences = re.split('[.?!]+', text)
     words_count = sum(len(s.split()) for s in sentences)
     return words_count / len(sentences)
+
+def avg_word_length(text):
+    words = re.findall('[a-zA-Z0-9]+', text)
+    word_lengths = [len(w) for w in words]
+    return sum(word_lengths) / len(words)
