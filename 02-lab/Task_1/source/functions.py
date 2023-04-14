2ba28bee843ac69df03adc586496b127a255648d
ZaharKaras <zahar.karas@gmail.com>
2023-04-14 17:20:17 +0300

    Feat: added avg sentence length

--- 02-lab/Task_1/source/functions.py
+++ 02-lab/Task_1/source/functions.py
@@ -6,4 +6,7 @@ def count_sentences(text):
 def count_nondeclarative_sentences(text):
     return len(re.findall(r'[?!][^.?!]*', text))
 
-
+def avg_sentence_length(text):
+    sentences = re.split('[.?!]+', text)
+    words_count = sum(len(s.split()) for s in sentences)
+    return words_count / len(sentences)
