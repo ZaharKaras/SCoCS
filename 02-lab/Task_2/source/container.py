ec0fddc77999c12184b37e694aee03fab1a76227
ZaharKaras <zahar.karas@gmail.com>
2023-04-20 02:46:22 +0300

    Update container

--- 02-lab/Task_2/source/container.py
+++ 02-lab/Task_2/source/container.py
@@ -2,9 +2,11 @@ import re
 import json
 
 class SetContainer:
-    def __init__(self, user_name: str):
+    def __init__(self, user_name: str, items=None):
         self.container = set()
         self.current_user = user_name
+        if items:
+            self.container.update(items)
 
     def add(self, *keys):
         for key in keys:
@@ -35,25 +37,53 @@ class SetContainer:
 
     def save(self, filename = '/home/karas/studying/4-semester/SCoCS/02-lab/data.json'):
         data = {
-        "container": list(self.container),
-        "current_user": self.current_user
-        }   
+            self.current_user: {
+                "container": list(self.container)
+            }
+        }
+
         try:
-            with open(filename, "r") as f:
+            with open(filename, 'r') as f:
                 existing_data = json.load(f)
         except (FileNotFoundError, json.decoder.JSONDecodeError):
-                existing_data = {}
+            existing_data = {}
 
-        existing_data.update(data)
-        with open(filename, "w") as f:
+        if self.current_user in existing_data:
+            existing_data[self.current_user].update(data[self.current_user])
+        else:
+            existing_data.update(data)
+
+        with open(filename, 'w') as f:
             json.dump(existing_data, f, indent=4)
 
-    def load(self, filename = 'data.json'):
+    def load(self, filename = '/home/karas/studying/4-semester/SCoCS/02-lab/data.json'):
         with open(filename, "r") as f:
             data = json.load(f)
-        self.container = set(data["container"])
-        self.current_user = data["current_user"]
+
+        for user, user_data in data.items():
+            if user == self.current_user:
+                loaded_container = user_data.get("container", [])
+                self.container.update(loaded_container)
+                return
+        print("No such user")
+
 
     def switch(self, user):
+        response = input("Do you want to save the container (y/n)")
+        if response == "y":
+            self.save()
+        
         self.current_user = user
+        self.container = set()
+        try:
+            with open('/home/karas/studying/4-semester/SCoCS/02-lab/data.json', 'r') as f:
+                data = json.load(f)
+        except json.decoder.JSONDecodeError:
+            print(f"Error: file  is empty or not valid JSON")
+            return
+    
+        for cur_user, user_data in data.items():
+            if cur_user == user:
+                self.container = user_data.get("container", [])                
+
         print(f"Switched to user {user}")
