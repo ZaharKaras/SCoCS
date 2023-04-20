721503d67f0270df3e3ec7adfd59b3d76a5c8d16
ZaharKaras <zahar.karas@gmail.com>
2023-04-20 10:42:12 +0300

    Added test for n_grams

--- 02-lab/Task_1/source/test.py
+++ 02-lab/Task_1/source/test.py
@@ -19,5 +19,8 @@ class TestFunctions(unittest.TestCase):
         text = "Hello! How are you? I'm doing well."
         self.assertAlmostEqual(functions.avg_word_length(text), 25/8)
 
+    def test_top_k_ngrams(self):
+        self.assertEqual(functions.top_k_ngrams('Hello, Maks!'), {'Hell': 1, 'ello': 1, 'llo,': 1, 'lo, ': 1, 'o, M': 1, ', Ma': 1, ' Mak': 1, 'Maks': 1, 'aks!': 1})
+        
 if __name__ == '__main__':
     unittest.main()
